8fe474bcbf692598dba2b296f00c07ed55c56b84
YisusC <donadomolinares@gmail.com>
2025-03-31 15:34:38 -0500

    commit
    commit

--- vue.config.js
+++ vue.config.js
@@ -1,30 +1,20 @@
 const titulo = require('./src/config/titulo')
-
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
     ? '/73311548_CF02_CULTIVO_DE_PLATANO/'
     : '/',
-    
   css: {
     loaderOptions: {
       sass: {
+        implementation: require('sass'),
         additionalData: `@import "@/styles/_styles.sass";`
       }
     }
   },
-  
   chainWebpack: config => {
     config.plugin('html').tap(args => {
       args[0].title = titulo
       return args
     })
   },
-  
-  configureWebpack: {
-    performance: {
-      hints: false,
-      maxAssetSize: 1048576 * 5, // 5MB
-      maxEntrypointSize: 1048576 * 5
-    }
-  }
 }
